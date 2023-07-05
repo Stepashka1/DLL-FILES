8a8ce67f7ca7dddaba555be902db30fe7135be2e
Рыкова Мария <RykovaMarii@yandex.ru>
2023-07-05 11:29:43 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Product.cs
@@ -0,0 +1,27 @@
+﻿// Product.cs Выполнил студент группы 34ИС Родионов Степан
+using System;
+
+namespace RodionovStepan34is.Library
+{
+    public class Product : IComparable
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine($"Product: {Name}, Number: {Number}");
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Product otherProduct = obj as Product;
+            if (otherProduct != null)
+                return this.Number.CompareTo(otherProduct.Number);
+            else
+                throw new ArgumentException("Object is not a Product");
+        }
+    }
+}
---
+++ ClassLibrary1/Store.cs
@@ -0,0 +1,27 @@
+﻿// Store.cs Выполнил студент группы 34ИС Родионов Степан
+using System;
+using System.Collections.Generic;
+
+namespace RodionovStepan34is.Library
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Store()
+        {
+            Products = new List<Product>();
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine($"Store: {Name}, Address: {Address}");
+            foreach (Product product in Products)
+            {
+                product.Show();
+            }
+        }
+    }
+}
